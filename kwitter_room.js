03c22973114a721c998b4296293ab24e0c69b498
anvvi123 <78200009+anvvi123@users.noreply.github.com>
2021-03-14 14:36:01 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBgrdk2fFL97OeFFjDr8GhQXkQwVfQDWWc",
+    authDomain: "app1-788b1.firebaseapp.com",
+    databaseURL: "https://app1-788b1-default-rtdb.firebaseio.com",
+    projectId: "app1-788b1",
+    storageBucket: "app1-788b1.appspot.com",
+    messagingSenderId: "358118376481",
+    appId: "1:358118376481:web:794e1aafaba2811af18f60",
+    measurementId: "G-PM6CL8K2D9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+ 
